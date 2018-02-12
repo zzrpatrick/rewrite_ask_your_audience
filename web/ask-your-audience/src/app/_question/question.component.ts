8121b87eae0e05be146cf19d56ab88024cbb346b
zzrpatrick <zzrpatrick@hotmail.com>
2018-02-12 15:38:08 -0600

    feb 12 update 2

---
+++ web/ask-your-audience/src/app/_question/question.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core'
+
+
+@Component({
+    selector: 'single-poll',
+    templateUrl: 'app/_question/quesiton.component.html',
+    styleUrls: ['app/_question/quesiton.component.css']
+  })
+
+  export class singleQuestion{
+      
+  }
